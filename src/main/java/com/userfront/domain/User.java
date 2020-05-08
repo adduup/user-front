2d7bd91368d11d5048d58edc547fabeb6b91299b
Adwait Uprety <hadocraftmusic@gmail.com>
2020-05-08 17:09:48 -0500

    initalizing cloud access

---
+++ src/main/java/com/userfront/domain/User.java
@@ -0,0 +1,135 @@
+package com.userfront.domain;
+
+import java.util.List;
+
+public class User {
+	private Long userId;
+
+	private String userName;
+
+	private String password;
+
+	private String fristName;
+
+	private String lastName;
+
+	private String email;
+
+	private String phoneNumber;
+
+	private boolean enabled = true;
+
+	private PrimaryAccount primaryAccount;
+
+	private SavingsAccount savingsAccount;
+
+	private List<Appointment> appointmentList;
+
+	private List<Recipient> recipientList;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFristName() {
+		return fristName;
+	}
+
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public PrimaryAccount getPrimaryAccount() {
+		return primaryAccount;
+	}
+
+	public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+		this.primaryAccount = primaryAccount;
+	}
+
+	public SavingsAccount getSavingsAccount() {
+		return savingsAccount;
+	}
+
+	public void setSavingsAccount(SavingsAccount savingsAccount) {
+		this.savingsAccount = savingsAccount;
+	}
+
+	public List<Appointment> getAppointmentList() {
+		return appointmentList;
+	}
+
+	public void setAppointmentList(List<Appointment> appointmentList) {
+		this.appointmentList = appointmentList;
+	}
+
+	public List<Recipient> getRecipientList() {
+		return recipientList;
+	}
+
+	public void setRecipientList(List<Recipient> recipientList) {
+		this.recipientList = recipientList;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", password=" + password + ", fristName="
+				+ fristName + ", lastName=" + lastName + ", email=" + email + ", phoneNumber=" + phoneNumber
+				+ ", enabled=" + enabled + "]";
+	}
+	
+	
+
+}
